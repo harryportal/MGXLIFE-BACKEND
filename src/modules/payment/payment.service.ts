832e5d2af038a7e971da48a4dd599e1731361293
Kuye Harry <dammykuye@gmail.com>
2023-05-19 03:36:00 +0100

    add methods for creating card and attaching to customer

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
 import { BadRequestError, InternalServerError } from "../../common/error";
+import ICard from "./payment.interface";
 
 export default class StripeService{
     private stripe:Stripe;
@@ -9,6 +10,46 @@ export default class StripeService{
         this.stripe =  new Stripe(this.secretKey,
             {apiVersion: '2022-11-15',  maxNetworkRetries: 3,  timeout: 1000})
     }
+    public createCustomer = async(cardDetails:ICard):Promise<Stripe.Customer>=>{
+        let customer: Stripe.Customer;
+        try {
+            customer = await this.stripe.customers.create({
+            
+            });
+        }catch(err:any){
+            throw new Error(`Failed to create subscription: ${err.message}`)
+        }
+        return customer;
+    }
+
+
+    public createPaymentMethod = async(cardToken: string): Promise<Stripe.PaymentMethod> {
+        try {
+          const paymentMethod = await this.stripe.paymentMethods.create({
+            type: 'card',
+            card: {
+             /*This card token is gotten when the client uses stripe.js to tokenise the 
+             card details actually: should have used in the other project lol!*/
+              token: cardToken,  
+            },
+          });
+    
+          return paymentMethod;
+        } catch (error) {
+          throw new Error(`Failed to create payment method: ${error.message}`);
+        }
+      }
+    
+      public attachPaymentMethodToCustomer = async(paymentMethodId: string, customerId: string): Promise<void> {
+        try {
+          await this.stripe.paymentMethods.attach(paymentMethodId, {
+            customer: customerId,
+          });
+        } catch (error) {
+          throw new Error(`Failed to attach payment method to customer: ${error.message}`);
+        }
+      }
+    
     
     public createSubscription = async(customerId: string, priceId: string): Promise<Stripe.Subscription>=>{
     try {
