a5ffa7ad9c61f13db0c759d6e44ac7beae927a45
Kuye Harry <dammykuye@gmail.com>
2023-05-22 10:15:20 +0100

    remove logs

--- dist/modules/referal/referral.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class referralController {
-}
-exports.default = referralController;
-referralController.createProduct = (req, res) => {
-    return res.status(200);
-};
--- dist/utils/pagination/page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const current_page = (req) => {
-    let pagenumber = Number(req.query.page) || 1;
-    const page_size = Number(process.env.PAGE_SIZE);
-    const skip = (pagenumber - 1) * page_size;
-    return [page_size, skip];
-};
-exports.default = current_page;
