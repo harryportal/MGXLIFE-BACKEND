ba69d98396479ce86e206b4e2be8942ee4fb918c
Kuye Harry <dammykuye@gmail.com>
2023-06-18 06:03:56 +0100

    edit verify_email url

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -117,7 +117,7 @@ export default class AuthService {
 
     public sendVerificationMail = async(firstname:string, email:string)=>{
         const verificationToken = createVerificationToken(email);
-        const verifyEmailUrl = `${process.env.FRONTENDURL}/verify_email.php/?token=${verificationToken}`;
+        const verifyEmailUrl = `${process.env.FRONTENDURL}/verify_email.php?token=${verificationToken}`;
         const mailtemplate = completeprofileTemplate(firstname, verifyEmailUrl);
         await this.mailService.sendMail({to:email, subject: "Verify Your Email Address", html:mailtemplate})
     }
