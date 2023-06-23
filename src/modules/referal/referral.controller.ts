ee4a542f3d5e92731b1390377b99ffb587e1f3f0
Kuye Harry <dammykuye@gmail.com>
2023-06-23 07:23:41 +0100

    remove dummy folders

--- src/modules/referal/referral.controller.ts
+++
@@ -1,9 +0,0 @@
-import { AuthRequest } from "../auth/auth.interface";
-import { Response } from "express";
-
-
-export default class referralController {
-    static createProduct = (req:AuthRequest, res:Response)=>{
-        return res.status(200);
-    }
-}
