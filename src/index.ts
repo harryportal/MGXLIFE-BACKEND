42a03d0f682eb231897c7ce4ec91c6ef961fab7d
Kuye Harry <dammykuye@gmail.com>
2023-06-29 15:54:18 +0100

    fix reflect metadata bug

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import app from './app';
 import * as dotenv from 'dotenv';
 import logger from './utils/logging/winston';
--- src/modules/admin/admin.router.ts
+++ src/modules/admin/admin.router.ts
@@ -7,7 +7,7 @@ const adminRouter = Router();
 adminRouter.get("/distributors", AdminController.getAllDistributors);
 adminRouter.get("/orders", AdminController.getAllOrders)
 adminRouter.get("/products", AdminController.getAllProducts)
-adminRouter.post("/profile/update", AdminController.up)
+adminRouter.post("/profile/update", AdminController.updateProfile)
 
 
 export default adminRouter;
